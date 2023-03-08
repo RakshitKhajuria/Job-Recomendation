0758e761e6a4bbdf400d6f163b4baef9a3f0f54f
Prikshit <101416953+Prikshit7766@users.noreply.github.com>
2023-03-08 23:37:32 +0530

    Update config.py

--- JobRecommendation/config.py
+++ JobRecommendation/config.py
@@ -3,13 +3,14 @@ import pandas as pd
 import json
 from dataclasses import dataclass 
 import os
+import streamlit as st
+# st.write("DB username:", st.secrets["db_username"])
 
-class EnvironmentVariable:
-    mongo_db_url:str = os.getenv("MONGO_DB_URL")
 
-env_var = EnvironmentVariable()
-client = pymongo.MongoClient(env_var.mongo_db_url)
-print ("connection established")
-print(env_var.mongo_db_url)
+# class EnvironmentVariable:
+#     mongo_db_url:str = os.getenv("MONGO_DB_URL")
 
+# env_var = EnvironmentVariable()
+client = pymongo.MongoClient(st.secrets["MONGO_DB_URL"])
+# print ("connection established")
 
