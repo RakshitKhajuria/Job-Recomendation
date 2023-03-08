6f74b827b28453348151307c7a37aad90ff68e1c
Prikshit <101416953+Prikshit7766@users.noreply.github.com>
2023-03-08 23:39:29 +0530

    Update __init__.py

--- JobRecommendation/utils/__init__.py
+++ JobRecommendation/utils/__init__.py
@@ -60,7 +60,6 @@ def generateUniqueFileName():
 def show_pdf(encoded_pdf:str):
     try:
         embed_code =f'<iframe src="data:application/pdf;base64,{encoded_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-
         return embed_code
     except Exception as e:
         raise jobException(e, sys)
