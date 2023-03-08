5d10d12e1e04c4e9b86011e94a3ff31158a6aa5b
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-03-08 23:26:02 +0530

    files updated

--- JobRecommendation/text_preprocessing.py
+++ JobRecommendation/text_preprocessing.py
@@ -1,4 +1,8 @@
 import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('words')
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords 
 from string import punctuation
@@ -12,9 +16,7 @@ from  JobRecommendation.exception import jobException
 import os,sys
 import numpy as np
 import streamlit as st
-# nltk.download('wordnet')
-# nltk.download('stopwords')
-# nltk.download('words')
+
 @st.cache_data
 def nlp(x):
     try:
